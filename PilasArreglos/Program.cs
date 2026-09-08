bf1ff05e01454030a9bc8544cade5c64a6221186
Isaac <izaqui447@gmail.com>
2026-09-08 07:29:50 -0700

    Agregar archivos de proyecto.

---
+++ PilasArreglos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PilasArreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
