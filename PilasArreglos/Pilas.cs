91731b6681d3b43a1327b8a9e02d9f308dde70c4
Isaac <izaqui447@gmail.com>
2026-09-08 07:55:33 -0700

    Definiendo la creación del arreglo.

---
+++ PilasArreglos/Pilas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PilasArreglos
+{
+    internal class Pilas
+    {
+        string[] _pila;
+        int _tope;
+
+        public Pilas(int elementos)
+        {
+            _pila = new string[elementos];
+            _tope = 0;
+        }
+    }
+}
